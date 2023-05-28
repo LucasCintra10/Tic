44d532536eef4a3bc8cde980958a8f53f04713a3
LucasCintra10 <lucasrcintra10@hotmail.com>
2023-05-28 13:50:39 -0300

    Update

--- front-end/src/components/Table/Table.tsx
+++ front-end/src/components/Table/Table.tsx
@@ -1,24 +1,56 @@
-import { SyntheticEvent } from 'react';
-import api from '../../lib/axios';
-import './Table.css';
+import api from "../../lib/axios";
+import { useEffect, useState } from "react";
+import "./Table.css";
+import { TableData } from "../../models/table-data";
 
 function Table() {
+  const [tableData, setTableData] = useState<TableData[]>([]);
 
-    async function receberDados(){
-       
-        const retorno = await api.get('/consulta').then((response) => {
-            return response.data;
-        })
-        return retorno;
-    }
-    console.log(receberDados())
+  function getTableData() {
+    api.get("/consulta").then((response) => setTableData(response.data));
+  }
 
-    return (
-       
-        <div className="table-container">
-            
-        </div>
-    )
+  useEffect(() => {
+    getTableData();
+  }, []);
+
+  return (
+    <div className="table-container">
+      <table>
+        <thead className="table-header">
+          <tr>
+            <th>Placa</th>
+            <th>Descricao</th>
+            <th>Categoria</th>
+            <th>Data de Entrada</th>
+            <th>Estado</th>
+            <th>Valor</th>
+            <th>Status</th>
+            <th>Localizacao</th>
+          </tr>
+        </thead>
+
+        {tableData?.length > 0 ? (
+          tableData.map((data) => (
+            <tbody className="table-body">
+              <tr>
+                <td>{data.placa}</td>
+                <td>{data.descricao}</td>
+                <td>{data.nm_categoria}</td>
+                <td>{new Date(data.dataEntrada).toLocaleDateString()}</td>
+                <td>{data.estado}</td>
+                <td>{data.valor}</td>
+                <td>{data.status}</td>
+                <td>{data.nm_sala}</td>
+              </tr>
+            </tbody>
+          ))
+        ) : (
+          <p>Nenhum dado encontrado</p>
+        )}
+      </table>
+    </div>
+  );
 }
 
 export default Table;
